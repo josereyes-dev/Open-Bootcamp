14ccd3297dd9a275197eee43bfbd586bbf4a8cb0
josereyes <joseluisreyes0858@gmail.com>
2023-05-09 22:51:58 -0500

    añadimos carpeta de curso de js

---
+++ cursojs/introduccion/index.js
@@ -0,0 +1,11 @@
+//Este es el mensaje de bienvenida
+
+console.log("Esta es la puerta de entrada del proyecto")
+
+/*Este es el intermedio de nuestro proyecto
+Realizamos los chequeos necesarios
+*/
+
+//Este es el mensaje de despedida
+
+console.log("Adios")
